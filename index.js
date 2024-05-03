713d51502aa027aa78f7aceb65134bdcc138ba07
Al-Ameen Ameen <alameenameen3101@gmail.com>
2024-05-03 11:01:14 +0100

    updated routes with info

--- index.js
+++ index.js
@@ -7,8 +7,41 @@ const authenticateToken = require('./middleware/auth')
 const User = require('./models/userModel')
 
 app.use(express.json())
-app.get(('/me'), authenticateToken, async(req, res)=>{
-    const user = User.findById(req.user.id).select('-password')
+app.get('/', (req, res)=>{
+    routes = {
+        '/me' : {
+            'method' : 'get',
+            'function' : 'get current user information',
+        },
+        'auth/register' : {
+            'method' : 'post',
+            'function' : "register new users ",
+        },
+        'auth/login' :  {
+            'method' : 'post',
+            'function' : 'login new users' ,
+            'note' : 'note that both login and register send a jwt token so you should save that on the frontend for authentication and delete it when logging out ',
+        },
+        'api/task' : {
+            'method' : 'get',
+            'function' : "get all tasks ",
+        },
+        'api/task' : {
+            'method' : 'post',
+            'function' : "create new task ",
+        },
+        'api/task/id' : {
+            'method' : 'put',
+            'function' : "update a particular task ",
+        },
+        'api/task/id' : {
+            'method' : 'delete',
+            'function' : "delete a particular task ",
+        },
+    }
+})
+app.get('/me', authenticateToken, async(req, res)=>{
+    const user = await User.findById(req.user.id).select('-password')
     res.send(user)
 })
 app.use('/api/task', todoRouter )
